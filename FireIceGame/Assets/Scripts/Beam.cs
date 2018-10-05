ca5ee76bd92f268688d4fc4469a9c38e1a1e8c45
Ansou <anton.sodergren@gmail.com>
2018-10-05 20:46:12 +0200

    Added support for creating ice blocks

--- FireIceGame/Assets/Scripts/Beam.cs
+++ FireIceGame/Assets/Scripts/Beam.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 
-public class Beam : MonoBehaviour
+public abstract class Beam : MonoBehaviour
 {
-    [SerializeField] private readonly float range_;
+#region Variables
+
+    [SerializeField] private float range_;
     [SerializeField] private Transform beamParent_;
     [SerializeField] private ParticleSystem particleSystem_;
     [SerializeField] private string horizontalInput_;
@@ -11,44 +13,69 @@ public class Beam : MonoBehaviour
     [SerializeField] private float maxEnergy_;
     [SerializeField] private float energyDrainPerSec_;
     [SerializeField] private float energyRegPerSec_;
+    [SerializeField] private LayerMask raycastMask_;
 
     private Vector2 aimDirection_;
     private bool shooting_;
     private float energy_;
 
+#endregion
 
-	// Use this for initialization
-    private void Start ()
+#region Protected Methods
+
+    protected virtual void Start()
     {
         energy_ = maxEnergy_;
     }
-	
-	// Update is called once per frame
+
+    protected abstract void HitRay(RaycastHit2D hit);
+
+    protected abstract void StopHit();
+
+#endregion
+    
     private void Update ()
     {
 		ProcessInput();
-        ShootBeam();
+        if (CheckBeam())
+        {
+            ShootBeam();
+        }
+        else
+        {
+            StopHit();
+        }
 	}
 
     private void ProcessInput()
     {
-        var inputDir = new Vector2(Input.GetAxis(horizontalInput_), Input.GetAxis(verticalInput_));
+        var inputDir = new Vector2(Input.GetAxis(horizontalInput_), -Input.GetAxis(verticalInput_));
 
         aimDirection_ = inputDir.normalized;
         
         shooting_ = inputDir.magnitude > inputThreshold_;
     }
 
-    private void ShootBeam()
+    private bool CheckBeam()
     {
         var shouldShoot = shooting_ && energy_ > 0f;
         var emission = particleSystem_.emission;
         emission.enabled = shouldShoot;
-        if (!shouldShoot)
-        {
-            return;
-        }
 
-        beamParent_.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(-aimDirection_.y, aimDirection_.x) * Mathf.Rad2Deg);
+        return shouldShoot;
+    }
+
+    private void ShootBeam()
+    {
+        beamParent_.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(aimDirection_.y, aimDirection_.x) * Mathf.Rad2Deg);
+        var hit = Physics2D.Raycast(beamParent_.position, aimDirection_, range_, raycastMask_);
+        if (hit.collider)
+        {
+            HitRay(hit);
+        }
+        else
+        {
+            StopHit();
+        }
     }
 }

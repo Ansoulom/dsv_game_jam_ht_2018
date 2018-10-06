018d4a675a58fe03d71cdc53b954e106e13c06c8
Marcus M <maccus9@hotmail.com>
2018-10-06 19:40:23 +0200

    lives added

--- FireIceGame/Assets/Scripts/Killzone.cs
+++ FireIceGame/Assets/Scripts/Killzone.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Killzone : MonoBehaviour {
     public Transform respawn;
+    public Lives lives;
+    public float respawnTime = 1f;
 
+    private bool respawning;
+
+    private void Start () {
+        lives = FindObjectOfType<Lives>();
+    }
     private void OnTriggerEnter2D (Collider2D other) {
         if (other.CompareTag("Player")) {
-            other.transform.position = respawn.position;
+            other.gameObject.SetActive(false);
+            if(respawning == true) {
+                lives.GameOver();
+            }
+            lives.HurtPlayer();
+            respawning = true;
+            StartCoroutine(Respawn(other));
         }
+    }
+    private IEnumerator Respawn (Collider2D player) {
+        yield return new WaitForSeconds(respawnTime);
+        player.transform.position = respawn.position;
+        respawning = false;
+        player.gameObject.SetActive(true);
     }
 }

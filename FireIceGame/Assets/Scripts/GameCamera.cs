765f729e8443b4060bae0da5ba87fa12ab63d071
Ansou <anton.sodergren@gmail.com>
2018-10-06 16:19:12 +0200

    Added camera script and adjusted camera and background

---
+++ FireIceGame/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameCamera : MonoBehaviour 
+{
+#region Variables
+
+    [SerializeField] private Transform player1_;
+    [SerializeField] private Transform player2_;
+
+#endregion
+
+#region Private Methods
+
+    private void LateUpdate ()
+    {
+        var playerPos = (player1_.position + player2_.position) / 2f;
+		transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+	}
+
+#endregion
+}

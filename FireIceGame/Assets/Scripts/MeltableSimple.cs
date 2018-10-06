7f7e71ff163d5a8d6ca07f2fe97d738136901fea
Ansou <anton.sodergren@gmail.com>
2018-10-06 19:43:57 +0200

    Added a MeltableSimple script

---
+++ FireIceGame/Assets/Scripts/MeltableSimple.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MeltableSimple : IceSurface
+{
+    protected override void Update()
+    {
+        if (fireTick_)
+        {
+            if (meltTimer_.Update(Time.deltaTime))
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            meltTimer_.Reset();
+        }
+    }
+}

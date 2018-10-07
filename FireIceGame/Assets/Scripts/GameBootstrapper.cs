09eebfd44687bd09b370ad15b1858c750d1d3c8d
Ansou <anton.sodergren@gmail.com>
2018-10-07 12:37:36 +0200

    Added music (play the Start scene to hear it)

---
+++ FireIceGame/Assets/Scripts/GameBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBootstrapper : MonoBehaviour
+{
+    [SerializeField] private string sceneName_ = "Main Menu";
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(sceneName_);
+    }
+}
